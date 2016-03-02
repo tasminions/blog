f25cdb56970e8a813329be46960f68fc27a77a9f
Elias <elias0789@gmail.com>
2016-03-02 11:37:06 +0000

    add more db tests, refactor db functions to use hash tables instead of lists

--- tests/db.tests.js
+++ tests/db.tests.js
@@ -6,7 +6,7 @@ var client, test = {};
 var dbUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 var dbNum = process.env.REDIS_DB || 16
 
-test.module1 = tape({
+test = tape({
   setup: function(t){
     client = redis.createClient(dbUrl)
     client.select(dbNum, function(){
@@ -21,7 +21,7 @@ test.module1 = tape({
   }
 })
 
-test.module1("testing if we can create a blog post", function(t){
+test("testing if we can create a blog post", function(t){
   var newBlogPost = {
     "title": "me",
     "author": "me",
@@ -30,8 +30,38 @@ test.module1("testing if we can create a blog post", function(t){
     "comments": [],
     "likes": 0
   }
-  db.createBlogPost(client, newBlogPost, function(error, reply){
+  t.plan(4)
+
+  db.createBlogPost(client, 7, newBlogPost, function(error, reply){
     t.ok(! error, "assert no error")
     t.ok(reply, "assert post has been stored")
+    client.HGETALL("posts", function(error, reply){
+      var expected = JSON.stringify(newBlogPost);
+      t.ok(! error, "no error")
+      t.equal(expected, reply[7], "checked that the new BlogPost has been stored!")
+    })
+  })
+})
+
+test("test can edit blog post", function(t) {
+  var blogPost = {
+    "title": "me",
+    "author": "me",
+    "date": "20357",
+    "body": "foahew9hqor",
+    "comments": [],
+    "likes": 0
+  }
+  var editedPostBody = "fnidubgqoniibcauhfq"
+  t.plan(3)
+  db.createBlogPost(client, 7, blogPost, function(err, __) {
+    t.ok(! err, "no error")
+
+    db.editBlogPost(client, 7, 'body', editedPostBody, function(_, __) {
+      client.HGET('posts', 7, function(error, reply) {
+        t.ok(! error, "no error")
+        t.equal(JSON.parse(reply).body, editedPostBody, "Assert has been edited")
+      })
+    })
   })
 })
